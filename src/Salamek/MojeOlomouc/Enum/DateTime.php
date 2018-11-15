7aa2aadde2860bc809987f9fc74d821615380595
Adam Schubert <adam.schubert@sg1-game.net>
2018-11-15 01:36:56 +0100

    DateTime format hack

---
+++ src/Salamek/MojeOlomouc/Enum/DateTime.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+namespace Salamek\MojeOlomouc\Enum;
+
+/**
+ * Class DateTime
+ * @package Salamek\MojeOlomouc\Enum
+ * @FIXME This must be fixed on API side and replaced by standard \DateTime::ISO8601
+ */
+class DateTime
+{
+    const NOT_A_ISO8601 = 'Y-m-d H:i:s';
+}
